4d87c237e33caee6417d1a94695512b26983e261
İlkgül Seray Dündar <129208512+seraydundar@users.noreply.github.com>
2025-03-04 14:21:25 +0300

    Update routing.py

--- game/routing.py
+++ game/routing.py
@@ -4,6 +4,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'^ws/lobby/(?P<lobby_id>\d+)/$', consumers.LobbyConsumer.as_asgi()),
-    # Oyun ekranı vs. isterseniz:
     re_path(r'^ws/game/(?P<game_id>\d+)/$', consumers.GameConsumer.as_asgi()),
 ]
