fa4a10dd61edb937ace1ba5a7a5670978439a8ad
İlkgül Seray Dündar <129208512+seraydundar@users.noreply.github.com>
2025-03-04 14:16:52 +0300

    Update urls.py

--- tabu_project/urls.py
+++ tabu_project/urls.py
@@ -4,7 +4,7 @@ from game import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.welcome_view, name="welcome"),  # Root URL artık welcome_view'a yönlendiriyor.
+    path('', views.welcome_view, name="welcome"),  # Root URL welcome_view'a yönlendiriyor.
     path('home/', views.home_view, name="home"),
     path('accounts/login/', views.login_view, name='login'),
     path('register/', views.register_view, name="register"),
@@ -18,17 +18,19 @@ urlpatterns = [
     path('login/', views.login_view, name="login"),
     path('mark_ready/', views.mark_ready, name="mark_ready"),
     path('toggle_ready/<int:lobby_id>/', views.toggle_ready, name="toggle_ready"),
-    #path('waiting_room/<int:lobby_id>/', views.waiting_room, name='waiting_room'),
     path('logout/', views.custom_logout, name="logout"),
+    
     # Arkadaşlık ve bildirim yolları
     path('add_friend/', views.add_friend, name="add_friend"),
     path('accept_request/<int:request_id>/', views.accept_request, name='accept_request'),
     path('reject_request/<int:request_id>/', views.reject_request, name='reject_request'),
     path('notifications/', views.notifications_view, name="notifications"),
+    
     # Oyun ekranları
     path('explainer_screen/<int:lobby_id>/', views.explainer_screen, name='explainer_screen'),
     path('teammate_screen/<int:lobby_id>/', views.teammate_screen, name='teammate_screen'),
     path('opponent_screen/<int:lobby_id>/', views.opponent_screen, name='opponent_screen'),
+    
     # Oyundan çıkma ve lobiye davet
     path('leave_game/<int:lobby_id>/', views.leave_game, name='leave_game'),
     path('invite_lobby/<int:lobby_id>/', views.invite_lobby, name='invite_lobby'),
@@ -38,7 +40,7 @@ urlpatterns = [
     path('toggle_round_ready/<int:lobby_id>/', views.toggle_round_ready, name='toggle_round_ready'),
     path('poll_round_ready/<int:lobby_id>/', views.poll_round_ready, name='poll_round_ready'),
     path('game_over/', views.game_over, name='game_over'),
-    #path('new_explainer/<int:lobby_id>/', views.new_explainer, name='new_explainer'),
+    
 ]
 
 
